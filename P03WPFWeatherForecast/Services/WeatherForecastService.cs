66c0ab0a0cb089724cd636e78469b5ced757bb6b
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-12 10:17:21 +0100

    q

---
+++ P03WPFWeatherForecast/Services/WeatherForecastService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace P03WPFWeatherForecast.Services
+{
+    class WeatherForecastService
+    {
+        const string url = $"https://www.google.com/search?q=pogoda";
+        const string regexTemplate = "<div class=\"BNeawe iBp4i AP7Wnd\">(-{0,1}\\d{1,3}).[CF]<\\/div>";
+
+        public int GetTemperature(string city)
+        {                    
+                WebClient wc = new WebClient();
+                string data = wc.DownloadString(url + " " + city);
+                try
+                {
+                    Regex rx = new Regex(regexTemplate);
+                    Match match = rx.Match(data);
+
+                    string result = match.Groups[1].Value;
+
+                    return Convert.ToInt32(result);
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Nie udało się pobrać temperatury");               
+                }
+            }
+        }
+    }
+
